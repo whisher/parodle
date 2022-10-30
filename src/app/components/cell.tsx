f1bfe549b846a0f6513339475b3f023f2c299462
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2022-10-30 09:23:18 +0100

    2 attempt

--- src/app/components/cell.tsx
+++ src/app/components/cell.tsx
@@ -8,42 +8,44 @@ export interface CellProps {
 }
 const Cell: React.FC<CellProps> = ({ char, i, match }) => {
 	let classNameContainer =
-		'[perspective:1000px] h-11 w-11 lg:h-12 lg:w-12 rounded-2xl border border-trasparent text-3xl text-white font-bold uppercase overflow-hidden';
+		'[perspective:1000px] h-11 w-11 lg:h-14 lg:w-14 rounded-2xl text-3xl text-white font-bold uppercase overflow-hidden';
 	let classNameTrasform =
-		'[transform-style:preserve-3d] w-full h-full relative transition-all duration-1000';
+		'[transform-style:preserve-3d] w-full h-full relative overflow-hidden bg-gray-400 transition-all duration-[5000ms]';
 	let classNameSpan1 =
-		'[backface-visibility:hidden] absolute w-full h-full flex justify-center items-center';
-	let classNameSpan2 = `${classNameSpan1} bg-gray-400 text-bck/80`;
+		'[backface-visibility:hidden] absolute z-10 w-full h-full flex justify-center items-center';
+	let classNameSpan2 = `${classNameSpan1} bg-gray-400 text-bck/80 transition duration-[5000ms] scale-0`;
 	if (match === IsMatch.OK) {
-		classNameContainer += ' !border-lime-400';
+		classNameContainer += '';
 		classNameTrasform += ' rotate-180';
 		classNameSpan1 += '';
-		classNameSpan2 += ' !bg-green-400 !border-lime-400 rotate-180 !scale-100';
+		classNameSpan2 += ' rounded-2xl !bg-green-400 rotate-180 !scale-100';
 	} else if (match === IsMatch.IN_THE_SOLUTION) {
 		classNameContainer += ' !border-amber-400';
 		classNameTrasform += ' rotate-180';
 		classNameSpan1 += '';
-		classNameSpan2 += ' !bg-amber-400 !border-amber-400 rotate-180 !scale-100';
+		classNameSpan2 += ' rounded-2xl !bg-amber-400 rotate-180 !scale-100';
 	} else if (match === IsMatch.WRONG) {
 		classNameContainer += ' !border-red-400';
 		classNameTrasform += ' rotate-180';
 		classNameSpan1 += '';
-		classNameSpan2 += ' !bg-red-400 !border-red-400 rotate-180 !scale-100';
+		classNameSpan2 += ' rounded-2xl !bg-red-400 rotate-180 !scale-100';
 	} else if (match === IsMatch.TO_CHECK) {
-		classNameContainer += ' bg-gray-400';
-		classNameSpan2 += ' !text-bck/80 !scale-100';
+		classNameContainer += ' ';
+		classNameSpan2 += ' !text-bck/80 bg-gray-400 !scale-100';
 	} else {
 		classNameContainer += ' bg-gray-400';
 	}
 
 	return (
-		<div className={classNameContainer}>
-			<div className={classNameTrasform} style={{ transitionDuration: `${i * 300}ms` }}>
-				<div className={classNameSpan1}>
-					<span className="">{char}</span>
-				</div>
-				<div className={classNameSpan2}>
-					<span className="">{char}</span>
+		<div className="bg-bck overflow-hidden relative z-0">
+			<div className={classNameContainer}>
+				<div className={classNameTrasform} style={{ transitionDuration: `${i * 300}ms` }}>
+					<div className={classNameSpan1}>
+						<span className="">{char}</span>
+					</div>
+					<div className={classNameSpan2}>
+						<span className="">{char}</span>
+					</div>
 				</div>
 			</div>
 		</div>
