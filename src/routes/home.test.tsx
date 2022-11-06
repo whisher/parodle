a900d5cbadf89b8b9c3b2b72806af00b26dffbe8
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2022-11-06 16:43:17 +0100

    testing keyboard by buttons

--- src/routes/home.test.tsx
+++ src/routes/home.test.tsx
@@ -6,107 +6,141 @@ import { ROW_LEN } from '../app/constants';
 import { Home } from './home';
 
 const WORDS = ['abaco'];
-test('Home', async () => {
-	const initialState: RowsState = {
-		solution: WORDS[Math.floor(Math.random() * WORDS.length)],
-		guesses: '',
-		invalidWord: '',
-		rows: [
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			},
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			},
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			},
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			},
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			},
-			{
-				guesses: Array(ROW_LEN).fill(''),
-				matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
-				isValidWord: false
-			}
-		],
-		words: WORDS,
-		keyboardKeysStatus: {}
-	};
-	renderWithProviders(<Home />, {
-		preloadedState: {
-			table: initialState
+const initialState: RowsState = {
+	solution: WORDS[Math.floor(Math.random() * WORDS.length)],
+	guesses: '',
+	invalidWord: '',
+	rows: [
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
+		},
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
+		},
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
+		},
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
+		},
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
+		},
+		{
+			guesses: Array(ROW_LEN).fill(''),
+			matches: Array(ROW_LEN).fill(IsMatch.NOT_SET),
+			isValidWord: false
 		}
-	});
+	],
+	words: WORDS,
+	keyboardKeysStatus: {}
+};
+describe('Home', () => {
+	test('Window Type', async () => {
+		renderWithProviders(<Home />, {
+			preloadedState: {
+				table: initialState
+			}
+		});
 
-	expect(await screen.findByTestId('table')).toBeInstanceOf(HTMLElement);
+		expect(await screen.findByTestId('table')).toBeInstanceOf(HTMLElement);
 
-	fireEvent.keyDown(window, {
-		key: 'q'
-	});
-	fireEvent.keyDown(window, {
-		key: 'w'
-	});
-	fireEvent.keyDown(window, {
-		key: 'e'
-	});
-	fireEvent.keyDown(window, {
-		key: 'r'
-	});
-	fireEvent.keyDown(window, {
-		key: 't'
-	});
+		fireEvent.keyDown(window, {
+			key: 'q'
+		});
+		fireEvent.keyDown(window, {
+			key: 'w'
+		});
+		fireEvent.keyDown(window, {
+			key: 'e'
+		});
+		fireEvent.keyDown(window, {
+			key: 'r'
+		});
+		fireEvent.keyDown(window, {
+			key: 't'
+		});
 
-	expect(screen.getAllByText((content, element) => content.includes(`qwert`))).toHaveLength(2);
+		expect(screen.getAllByText((content, element) => content.includes(`qwert`))).toHaveLength(2);
 
-	fireEvent.keyDown(window, {
-		key: 'Backspace'
-	});
-	fireEvent.keyDown(window, {
-		key: 'Backspace'
-	});
-	fireEvent.keyDown(window, {
-		key: 'Backspace'
-	});
-	fireEvent.keyDown(window, {
-		key: 'Backspace'
-	});
-	fireEvent.keyDown(window, {
-		key: 'Backspace'
-	});
-	fireEvent.keyDown(window, {
-		key: 'a'
-	});
-	fireEvent.keyDown(window, {
-		key: 'b'
-	});
-	fireEvent.keyDown(window, {
-		key: 'a'
-	});
-	fireEvent.keyDown(window, {
-		key: 'c'
-	});
-	fireEvent.keyDown(window, {
-		key: 'o'
-	});
+		fireEvent.keyDown(window, {
+			key: 'Backspace'
+		});
+		fireEvent.keyDown(window, {
+			key: 'Backspace'
+		});
+		fireEvent.keyDown(window, {
+			key: 'Backspace'
+		});
+		fireEvent.keyDown(window, {
+			key: 'Backspace'
+		});
+		fireEvent.keyDown(window, {
+			key: 'Backspace'
+		});
+		fireEvent.keyDown(window, {
+			key: 'a'
+		});
+		fireEvent.keyDown(window, {
+			key: 'b'
+		});
+		fireEvent.keyDown(window, {
+			key: 'a'
+		});
+		fireEvent.keyDown(window, {
+			key: 'c'
+		});
+		fireEvent.keyDown(window, {
+			key: 'o'
+		});
 
-	expect(await screen.findAllByText((content, element) => content.includes('""'))).toHaveLength(2);
-	expect(await screen.findByTestId('modal-success')).toBeInstanceOf(HTMLElement);
+		expect(await screen.findAllByText((content, element) => content.includes('""'))).toHaveLength(
+			2
+		);
+		expect(await screen.findByTestId('modal-success')).toBeInstanceOf(HTMLElement);
 
-	fireEvent.click(screen.getByTestId('modal-button'));
+		fireEvent.click(screen.getByTestId('modal-button'));
 
-	expect(() => screen.getByTestId('modal-success')).toThrow();
+		expect(() => screen.getByTestId('modal-success')).toThrow();
+
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /a/i
+			})
+		);
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /b/i
+			})
+		);
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /a/i
+			})
+		);
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /c/i
+			})
+		);
+		fireEvent.click(
+			screen.getByRole('button', {
+				name: /o/i
+			})
+		);
+		expect(await screen.findByTestId('modal-success')).toBeInstanceOf(HTMLElement);
+		fireEvent.click(screen.getByTestId('modal-button'));
+
+		expect(() => screen.getByTestId('modal-success')).toThrow();
+	});
 });
